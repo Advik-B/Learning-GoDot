b0715f4ffdc7d09d0130ef08242e9fd5d959b955
Advik <advik.b@gmail.com>
2022-02-16 13:34:30 +0530

    Add player

---
+++ 2d_game/Player.cs
@@ -0,0 +1,16 @@
+using Godot;
+using System;
+
+public class Player : Area2D
+{
+
+	[Export]
+	public int speed = 400;
+	public Vector2 ScreenSize;
+    public override void _Ready()
+    {
+        ScreenSize = GetViewportRect().Size;
+    }
+	
+
+}
