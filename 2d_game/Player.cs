494cfc4d21460ddf7751a7a989a24844d01dcb0e
Advik <advik.b@gmail.com>
2022-02-16 14:00:58 +0530

    add basic animations

--- 2d_game/Player.cs
+++ 2d_game/Player.cs
@@ -11,6 +11,39 @@ public class Player : Area2D
 	{
 		ScreenSize = GetViewportRect().Size;
 	}
+	public override void _Process(float delta)
+	{
+		var velocity = Vector2.Zero;
+		if (Input.IsActionPressed("move_right"))
+		{
+			velocity.x += 1;
+		}
+		if (Input.IsActionPressed("move_left"))
+		{
+			velocity.x -= 1;
+		}
+		if (Input.IsActionPressed("move_up"))
+		{
+			velocity.y -= 1;
+		}
+		if (Input.IsActionPressed("move_down"))
+		{
+			velocity.y += 1;
+		}
+		var animated_sprite = GetNode<AnimatedSprite>("AnimatedSprite");
+		if (velocity.Length() > 0)
+		{
+			velocity = velocity.Normalized() * speed;
+			animated_sprite.Play();
+		}
+		else
+		{
+			animated_sprite.Stop();
+		}
+		
+
+	}
+	
 	
 
 
